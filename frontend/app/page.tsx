2e68e315c4726201ab324383160fe6b55a51588c
Anderson <andibaez@soy.sena.edu.co>
2026-05-23 16:02:11 -0500

    Revert "frontend de VetNova"
    This reverts commit 8ed451c9a54a604104457edcb7eda335ae5cf142.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
           alt="Next.js logo"
           width={100}
           height={20}
-          style={{ width: "auto", height: "auto" }}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -48,7 +47,6 @@ export default function Home() {
               alt="Vercel logomark"
               width={16}
               height={16}
-              style={{ width: "auto", height: "auto" }}
             />
             Deploy Now
           </a>
